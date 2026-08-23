686fca463798f145f68f11c37a93657d7e8121c5
chohanraza81-rgb <chohanraza81@gmail.com>
2026-08-24 02:20:08 +0500

    Update sitemap.ts

--- src/lib/sitemap.ts
+++ src/lib/sitemap.ts
@@ -42,7 +42,7 @@ export async function generateSitemapXML(): Promise<string> {
 
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>${baseUrl}</loc><priority>1.0</url>
+  <url><loc>${baseUrl}</loc><priority>1.0</priority></url>
   ${postUrls}
   ${programmaticUrls}
 </urlset>`;
